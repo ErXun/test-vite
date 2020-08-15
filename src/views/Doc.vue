4da186aef15de7cf2a33efe334d6324aed8e17ed
zhuangfengyuan <zhuangfengyuan@jsservice.cn>
2020-08-15 18:23:12 +0800

    实现 aside 的切换

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,19 +1,59 @@
 <template>
   <Tobnav />
   <div class="content">
-    <aside>aside</aside>
+    <aside v-if="asideVisible">
+      <ul>
+        <li>
+          <h3>组件列表</h3>
+        </li>
+        <li>
+          <router-link to>Switch 组件</router-link>
+        </li>
+        <li>
+          <router-link to>Button 组件</router-link>
+        </li>
+        <li>
+          <router-link to>Dialog 组件</router-link>
+        </li>
+        <li>
+          <router-link to>Tabs 组件</router-link>
+        </li>
+      </ul>
+    </aside>
     <main>main</main>
   </div>
 </template>
 
-<script>
+<script lang='ts'>
 import Tobnav from "../components/Tobnav.vue";
+import { inject, Ref } from "vue";
+
 export default {
   components: {
     Tobnav,
   },
+  setup() {
+    const asideVisible = inject<Ref<boolean>>("asideVisible");
+    return { asideVisible };
+  },
 };
 </script>
 <style scoped lang='scss'>
-
+.content {
+  display: flex;
+  width: 100%;
+  > aside {
+    width: 30%;
+    max-width: 260px;
+    min-width: 150px;
+    background-color: rgb(170, 212, 233);
+    padding: 10px 0 10px 15px;
+    > ul li {
+      margin: 10px 0;
+    }
+  }
+  > main {
+    width: 70%;
+  }
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <Tobnav />
   <div class="banner">
-    <h3>文字介绍</h3>
+    <h1>UI 框架</h1>
     <p class="actions">
-      <a href>GitHub</a>
-      <a href>开始</a>
+      <a href="https://github.com/">GitHub</a>
+      <router-link to='/doc'>开始</router-link>
     </p>
   </div>
 </template>
@@ -17,4 +17,27 @@ export default {
   },
 };
 </script>
+<style scoped lang='scss'>
+.banner {
+  padding: 100px 0;
+  background-color: rgb(137, 223, 145);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  > .actions {
+    padding: 10px 0;
+    > a {
+      display: inline-block;
+      margin: 0 8px;
+      padding: 0px 8px;
+      background-color: rgb(249, 250, 252);
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+    }
+  }
+}
+</style>
 
