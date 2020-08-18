f25ffe55d407e22d63f976fd326a4672e22f9469
zhuangfengyuan <zhuangfengyuan@jsservice.cn>
2020-08-18 15:05:43 +0800

    ex-button 的属性学习

---
+++ src/libs/ExButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :size="size">
+    <!-- 1.绑定全部属性 -->
+    <!-- <button v-bind="$attrs"> -->
+    <!-- 2.绑定单个属性/方法 -->
+    <!-- <button @click="onClick"> -->
+    <!-- 3.绑定除 size 之外的属性 -->
+    <button v-bind="rest">
+      <slot />
+    </button>
+  </div>
+</template>
+
+<script lang='ts'>
+export default {
+  inheritAttrs: false,
+  setup(props, context) {
+    // console.log({ ...props });
+    // console.log({ ...context.attrs });
+    // 使用 $attrs 或者 context.attrs 获取所有属性
+    const { size, ...rest } = context.attrs;
+    // const { size, onClick } = context.attrs;
+    return { size, rest };
+  },
+};
+</script>
--- src/views/content/Button.vue
+++ src/views/content/Button.vue
@@ -1,14 +1,31 @@
 <template>
-  <div>
-    Button
-  </div>
+  <ex-button 
+    @click="onClick" 
+    @focus="onFocus" 
+    @mouseover="onMouseover" 
+    size="small">我是按钮</ex-button>
 </template>
 
-<script>
-export default {
+<script lang='ts'>
+import ExButton from "../../libs/ExButton.vue";
 
-}
+export default {
+  components: {
+    ExButton,
+  },
+  setup() {
+    const onClick = () => {
+      console.log("onClick");
+    };
+    const onFocus = () => {
+      console.log("onFocus");
+    };
+    const onMouseover = () => {
+      console.log("onMouseover");
+    };
+    return { onClick, onFocus, onMouseover };
+  },
+};
 </script>
 <style scoped>
-
 </style>
