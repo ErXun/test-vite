4614cfb6e60f0b36a68a6ed733d524d6e69b8ad4
erxun <erxun.fighting@foxmail.com>
2020-09-20 11:04:30 +0800

    开始编写 tabs 组件

---
+++ src/libs/ExTab.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    tab
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+
+</style>
--- src/views/content/Tabs.vue
+++ src/views/content/Tabs.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
-    Tabs
+    tabs 组件
+    <ex-tabs>
+      <ex-tab title="tab1">内容1</ex-tab>
+      <ex-tab title="tab2">内容2</ex-tab>
+      <ex-tab title="tab3">内容3</ex-tab>
+    </ex-tabs>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import ExTabs from "../../libs/ExTabs.vue";
+import ExTab from "../../libs/ExTab.vue";
 export default {
-
-}
+  component: {
+    ExTabs,
+    ExTab,
+  },
+};
 </script>
 <style scoped>
-
 </style>
